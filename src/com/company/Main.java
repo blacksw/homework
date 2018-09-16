c8bcbeaaca3013ed0a500fee49fe3dfd0bed3e73
blacksw <sweetiki57@gmail.com>
2018-09-16 22:01:53 +0300

    about

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int pizza = 60;
+        int burger = 40;
+        int soup = 20;
+        Menu black = new Menu();
+        black.say("black",pizza);
+    }
+}
---
+++ src/com/company/Menu.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Menu {
+
+    public void say(String name,int kit){
+        System.out.println("Hello, "+ name +"\n"+"Your order amount is "+ kit);
+    }
+}
